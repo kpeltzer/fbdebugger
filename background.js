219dc31db76981c06c5247f4ea619b285739728a
Ken Peltzer <ken@kenpeltzer.com>
2013-06-05 00:12:46 -0400

    Initial commit of background.js

---
+++ background.js
@@ -0,0 +1,38 @@
+var FBDebugger = {
+	fb_endpoint : 'https://developers.facebook.com/tools/debug/og/object?q=',
+	page_url : '',
+
+	sendToFacebook : function(info, tab) {
+		var req = new XMLHttpRequest(),
+			url = info.pageUrl,
+			request_url = FBDebugger.fb_endpoint + encodeURIComponent(url);
+
+		FBDebugger.page_url = url;
+
+		req.open("GET", request_url, true);
+	    req.onload = FBDebugger.onLoad;
+	    req.onerror = function(e){
+	    	console.log(e);
+	    };
+	    req.send(null);
+	},
+
+	onLoad : function (e) {
+		var notification = webkitNotifications.createNotification(
+		  '',  
+		  'Facebook debug successful!',
+		  FBDebugger.page_url + ' was refreshed at Facebook.'
+		);
+
+		notification.show();
+	}
+}
+
+chrome.contextMenus.create({
+	"title": "Refresh page on Facebook",
+	"contexts": ["page"],
+	"id": "fbsend"
+});
+
+chrome.contextMenus.onClicked.addListener(FBDebugger.sendToFacebook);
+
